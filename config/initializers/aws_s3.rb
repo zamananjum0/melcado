3d0fd37c0b9c6f985b9761e5bc2c278bb88a8081
LucaDev13 <lucamelis12@gmail.com>
2017-03-15 21:10:36 +0000

    change url format for attachments

--- config/initializers/aws_s3.rb
+++ config/initializers/aws_s3.rb
@@ -10,8 +10,8 @@ attachment_config = {
   s3_headers:     { "Cache-Control" => "max-age=31557600" },
   s3_protocol:    '',
   bucket:         ENV['S3_BUCKET_NAME'],
-  url:            ":s3_domain_url",
-  s3_alias_url:   "cdn.melcado.com"
+  url:            ":s3_path_url",
+
 
   styles: {
       mini:     "48x48>",
